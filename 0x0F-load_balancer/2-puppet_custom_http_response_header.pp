0f76b7dc2640e0332521156a5fdb770328b3c3c3
MikeMalindzisa <machaweml@gmail.com>
2024-04-30 17:14:08 -0700

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,31 @@
-exec { 'add_header':
-  command  => 'sudo apt-get update;
-  sudo apt-get -y install nginx;
-  sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
-  sudo service nginx restart',
-  provider => 'shell',
+exec { 'update-system':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update-system'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+file_line { 'redirect-me':
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4/ permanent;',
+  match   => 'rewrite ^/redirect_me',
+  require => Package['nginx'],
+}
+
+file_line { 'add-http-header':
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'add_header X-Served-By $hostname;',
+  match   => 'rewrite ^/redirect_me',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  require => Package['nginx'],
 }
