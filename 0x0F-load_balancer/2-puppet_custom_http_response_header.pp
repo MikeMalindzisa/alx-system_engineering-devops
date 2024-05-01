4df2a0ad965d68b9577e9e8bea7099c7a99240fa
MikeMalindzisa <machaweml@gmail.com>
2024-04-30 17:26:20 -0700

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,31 +1,70 @@
-exec { 'update-system':
-  command => '/usr/bin/apt-get update',
-}
+#!/usr/bin/env puppet
+#setting up new server and nginx response hearder
 
-package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['update-system'],
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
-
-file_line { 'redirect-me':
-  path    => '/etc/nginx/sites-available/default',
-  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4/ permanent;',
-  match   => 'rewrite ^/redirect_me',
-  require => Package['nginx'],
-}
-
-file_line { 'add-http-header':
-  path    => '/etc/nginx/sites-available/default',
-  line    => 'add_header X-Served-By $hostname;',
-  match   => 'rewrite ^/redirect_me',
-  require => Package['nginx'],
+exec { 'update-and-install-nginx':
+  command => '/usr/bin/apt-get update -y -qq && /usr/bin/apt-get install nginx -y',
 }
 
 service { 'nginx':
   ensure  => 'running',
-  require => Package['nginx'],
+  require => Exec['update-and-install-nginx'],
 }
+
+exec { 'firewall-access':
+  command => '/usr/sbin/ufw allow "Nginx HTTP"',
+}
+
+file { '/var/www/html':
+  ensure => 'directory',
+  owner  => $USER,
+  group  => $USER,
+  mode   => '0755',
+}
+
+file { '/var/www/html/index.nginx-debian.html.bckp':
+  ensure  => 'present',
+  source  => '/var/www/html/index.nginx-debian.html',
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => 'file',
+  content => 'Hello World!',
+}
+
+file_line { 'redirect-setting':
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Service['nginx'],
+}
+
+file { '/var/www/html/error_404.html':
+  ensure  => 'file',
+  content => "Ceci n'est pas une page",
+}
+
+file_line { '404-page-setting':
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'error_page 404 /error_404.html;',
+  require => Service['nginx'],
+}
+
+file_line { 'custom-response-header':
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'add_header X-Served-By $hostname;',
+  match   => 'server_name',
+  require => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  require => [
+    File_line['redirect-setting'],
+    File_line['404-page-setting'],
+    File_line['custom-response-header'],
+  ],
+  subscribe => [
+    File['/var/www/html/index.nginx-debian.html'],
+    File['/var/www/html/error_404.html'],
+  ],
+}
+
