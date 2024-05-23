9d45a371242e6749bb6e1e1d52ba153c4e122ae3
MikeMalindzisa <machaweml@gmail.com>
2024-05-22 20:02:15 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,64 +1,24 @@
 #!/usr/bin/python3
 """
-Queries JSON Placeholder API to retrieve employee data.
+Employee data query
 """
 
-import requests
-import sys
-
-
-def get_employee_data(user_id):
-    """
-    Retrieves todo data and user's name from JSON Placeholder API.
-
-    Args:
-        user_id (int): ID of the user.
-
-    Returns:
-        dict: A dictionary containing the employee's name, number of tasks,
-              and number of completed tasks, along with the list of completed
-              tasks titles.
-    """
-    main_url = 'https://jsonplaceholder.typicode.com'
-    todo_url = f"{main_url}/user/{user_id}/todos"
-    name_url = f"{main_url}/users/{user_id}"
-
-    # Fetch todo and name data
-    todo_result = requests.get(todo_url).json()
-    name_result = requests.get(name_url).json()
-
-    # Count total number of tasks and completed tasks
-    total_tasks = len(todo_result)
-    completed_tasks = sum(1 for todo in todo_result if todo.get("completed"))
-
-    # Get employee name
-    employee_name = name_result.get("name")
-
-    # Get list of completed tasks titles
-    completed_tasks_titles = [todo.get("title") for todo in todo_result
-                              if todo.get("completed")]
-
-    return {
-        "name": employee_name,
-        "total_tasks": total_tasks,
-        "completed_tasks": completed_tasks,
-        "completed_tasks_titles": completed_tasks_titles
-    }
-
+from requests import get
+from sys import argv
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: {} <user_id>".format(sys.argv[0]))
-        sys.exit(1)
+    endpoint = 'https://jsonplaceholder.typicode.com'
+    to_do = endpoint + "/user/{}/todos".format(argv[1])
+    name_uri = endpoint + "/users/{}".format(argv[1])
+    res = get(to_do).json()
+    name_res = get(name_uri).json()
 
-    user_id = sys.argv[1]
-    try:
-        user_data = get_employee_data(user_id)
-        print("Employee {} is done with tasks ({}/{}):".format(
-                user_data["name"],
-                user_data["completed_tasks"],
-                user_data["total_tasks"]))
-        for title in user_data["completed_tasks_titles"]:
-            print("\t{}".format(title))
-    except requests.exceptions.RequestException as e:
-        print("Error: {}".format(e))
+    todo_num = len(res)
+    completed = len([todo for todo in res
+                         if todo.get("completed")])
+    name = name_res.get("name")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, completed, todo_num))
+    for todo in res:
+        if (todo.get("completed")):
+            print("\t {}".format(todo.get("title")))
