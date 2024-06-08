a987822473775ef8ae0164c28ee87730b92eebb9
MikeMalindzisa <machaweml@gmail.com>
2024-06-08 03:02:03 +0200

    removed a few comments

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,8 @@
 # Ensure correct PHP extension in wp-settings.php by replacing 'phpp' with 'php'
 
-# Define an exec resource to execute a command
-exec { 'fix_wordpress_extension':
-  # Command to replace 'phpp' with 'php' in wp-settings.php using sed
-  command   => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  provider  => 'shell',
+# Fixing Apache returning a 500 error
+
+exec { 'fix error':
+  provider => 'shell',
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
