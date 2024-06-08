455a151fc494876af0731fce2debc1551dbe473b
MikeMalindzisa <machaweml@gmail.com>
2024-06-08 02:58:48 +0200

    added provider

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,5 +3,6 @@
 # Define an exec resource to execute a command
 exec { 'fix_wordpress_extension':
   # Command to replace 'phpp' with 'php' in wp-settings.php using sed
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  command   => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  provider  => 'shell',
 }
